140ee407980b336b67905421bb5980ca55d82a1b
Kang-Hyesung <hyesung9797@gmail.com>
2023-09-29 11:34:05 +0900

    Version3

--- src/Room.java
+++ src/Room.java
@@ -1,10 +1,26 @@
 public class Room {
-    String roomNum;     // 객실 호수
-    String grade;       // 객실의 등급
-    String bedType;     // 침대 타입
-    int num;            // 객실의 사용가능 인원
-    int roomPrice;      // 객실 가격
-    boolean roomRe;     // 예약 여부
+    private String roomNum;     // 객실 호수
+    private String grade;       // 객실의 등급
+    private String bedType;     // 침대 타입
+    private int num;            // 객실의 사용가능 인원
+    private int roomPrice;      // 객실 가격
+    private boolean roomRe;     // 예약 여부
+
+    public String getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(String roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 
     public String getBedType() {
         return bedType;
@@ -12,6 +28,14 @@ public class Room {
 
     public void setBedType(String bedType) {
         this.bedType = bedType;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 
     public int getRoomPrice() {
@@ -29,15 +53,27 @@ public class Room {
     public void setRoomRe(boolean roomRe) {
         this.roomRe = roomRe;
     }
+
+    public void checkCon(int num){
+        if(this.num >= num)
+            System.out.println("인원수 : 가능");
+        else
+            System.out.println("인원수 : 불가능");
+    }
 }
 
 class Deluxe extends Room{
     {
-        roomNum = "101";
-        grade = "deluxe";
-        bedType = "twin";
-        num = 2;
-        roomPrice = 150000;
-        roomRe = false;
+        setGrade("Deluxe");
+        setBedType("twin");
+        setNum(2);
+        setRoomPrice(150000);
+        setRoomRe(false);
+    }
+
+    Deluxe(String roomNum, String bedType)
+    {
+        setRoomNum(roomNum);
+        setBedType(bedType);
     }
 }
